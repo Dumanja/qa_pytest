5ed9be229ad434d4c452bf643607a04aea5ecef4
topalovicmarko <marko.topalovic99@gmail.com>
2022-03-25 19:17:12 +0100

    New lessons - Pip

---
+++ pythonCourse/97.pyToExe.py
@@ -0,0 +1,6 @@
+# pip install pyinstaller
+# put the script and the converted icon in one folder
+# get to the desired directory in cmd
+# Type in cmd:
+# C:\Users\Topalovic\qa_pytest\pythonCourse\97.pyToExe>pyinstaller -F -w -i icon.ico clock.py
+# the application is located in the dist folder
